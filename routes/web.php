3049e7b772f383688b033143cdcf5288cbcc13fc
Claim <claim@MacBook-Pro-Claim.local>
2023-05-07 13:06:56 +0300

    New Web.php controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\BlockController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CouponController;
+use App\Http\Controllers\Admin\MerchantController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\PropertyController;
+use App\Http\Controllers\Admin\PropertyOptionController;
+use App\Http\Controllers\Admin\SkuController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\BasketController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Person\OrdersController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,32 +34,32 @@ Auth::routes([
     'verify'=>false
 ]);
 
-Route::get('locale/{locale}', '\App\Http\Controllers\HomeController@changeLocale')->name('locale');
+Route::get('locale/{locale}', [HomeController::class, 'changeLocale'])->name('locale');
 
-Route::get('currency/{currencyCode}', '\App\Http\Controllers\HomeController@changeCurrency')->name('currency');
+Route::get('currency/{currencyCode}', [HomeController::class, 'changeCurrency'])->name('currency');
 
-Route::get('logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('get-logout');
+Route::get('logout', [LoginController::class, 'logout'])->name('get-logout');
 
 Route::group(['middleware'=>'set_locale'], function(){
     Route::group(['middleware'=>'auth'], function(){
-        Route::get('admin', '\App\Http\Controllers\Admin\AdminController@admin')->name('admin');
+        Route::get('admin', [AdminController::class, 'admin'])->name('admin');
 
-        Route::get('admin/person/orders', '\App\Http\Controllers\Person\OrdersController@orders')->name('person-orders');
-        Route::get('admin/person/orders/{order_id}', '\App\Http\Controllers\Person\OrdersController@order')->name('person-order');
+        Route::get('admin/person/orders', [OrdersController::class, 'orders'])->name('person-orders');
+        Route::get('admin/person/orders/{order_id}', [OrdersController::class, 'order'])->name('person-order');
 
         Route::group(['middlewate'=>'is_admin'], function (){
-            Route::get('admin/orders', '\App\Http\Controllers\Admin\AdminController@orders')->name('admin-orders');
-            Route::get('admin/orders/{order_id}', '\App\Http\Controllers\Admin\AdminController@order')->name('admin-order');;
-            Route::resource('admin/categories', '\App\Http\Controllers\Admin\CategoryController');
-            Route::resource('admin/products', '\App\Http\Controllers\Admin\ProductController');
-            Route::resource('admin/products/{product}/skus', '\App\Http\Controllers\Admin\SkuController');
-            Route::resource('admin/properties', '\App\Http\Controllers\Admin\PropertyController');
-            Route::resource('admin/property_options', '\App\Http\Controllers\Admin\PropertyOptionController');
-            Route::resource('admin/coupons', '\App\Http\Controllers\Admin\CouponController');
-            Route::resource('admin/merchants', '\App\Http\Controllers\Admin\MerchantController');
-            Route::get('admin/merchant/{merchant}/update_token', '\App\Http\Controllers\Admin\MerchantController@updateToken')->name('merchants.update_token');
+            Route::get('admin/orders', [AdminController::class, 'orders'])->name('admin-orders');
+            Route::get('admin/orders/{order_id}', [AdminController::class, 'order'])->name('admin-order');;
+            Route::resource('admin/categories', CategoryController::class);
+            Route::resource('admin/products', ProductController::class);
+            Route::resource('admin/products/{product}/skus', SkuController::class);
+            Route::resource('admin/properties', PropertyController::class);
+            Route::resource('admin/property_options', PropertyOptionController::class);
+            Route::resource('admin/coupons', CouponController::class);
+            Route::resource('admin/merchants', MerchantController::class);
+            Route::get('admin/merchant/{merchant}/update_token', [MerchantController::class, 'updateToken'])->name('merchants.update_token');
 
-            Route::resource('admin/block', '\App\Http\Controllers\Admin\BlockController', [
+            Route::resource('admin/block', BlockController::class, [
                 'only' => ['index','create','show','store','destroy']
             ]);
         });
@@ -53,20 +67,20 @@ Route::group(['middleware'=>'set_locale'], function(){
 
     Route::get('/',[HomeController::class, 'index'])->name('index');
 
-    Route::get('products', '\App\Http\Controllers\ProductsController@allProducts')->name('products');
+    Route::get('products', [ProductsController::class, 'allProducts'])->name('products');
 
-    Route::get('categories', '\App\Http\Controllers\CategoriesController@categories')->name('categories');
-    Route::get('category/{category_name}', '\App\Http\Controllers\CategoriesController@category')->name('category');
+    Route::get('categories', [CategoriesController::class, 'categories'])->name('categories');
+    Route::get('category/{category_name}', [CategoriesController::class, 'category'])->name('category');
 
-    Route::get('product/{product_name}/{skus}', '\App\Http\Controllers\ProductController@sku')->name('sku');
+    Route::get('product/{product_name}/{skus}', [ProductController::class, 'sku'])->name('sku');
 
-    Route::get('basket','\App\Http\Controllers\BasketController@basket')->name('basket');
-    Route::post('basket/add/{sku}', '\App\Http\Controllers\BasketController@add')->name('basket-add');
-    Route::post('basket/remove/{sku}', '\App\Http\Controllers\BasketController@remove')->name('basket-remove');
-    Route::get('basket/place', '\App\Http\Controllers\BasketController@place')->name('basket-place');
-    Route::post('basket/save-order', '\App\Http\Controllers\BasketController@basketConfirm')->name('save-order');
+    Route::get('basket',[BasketController::class, 'basket'])->name('basket');
+    Route::post('basket/add/{sku}', [BasketController::class, 'add'])->name('basket-add');
+    Route::post('basket/remove/{sku}', [BasketController::class, 'remove'])->name('basket-remove');
+    Route::get('basket/place', [BasketController::class, 'place'])->name('basket-place');
+    Route::post('basket/save-order', [BasketController::class, 'basketConfirm'])->name('save-order');
 
-    Route::post('subscription/{sku}', '\App\Http\Controllers\HomeController@subscribe')->name('subscription');
+    Route::post('subscription/{sku}', [HomeController::class, 'subscribe'])->name('subscription');
 
-    Route::post('coupon', '\App\Http\Controllers\BasketController@setCoupon')->name('set-coupon');
+    Route::post('coupon', [BasketController::class, 'setCoupon'])->name('set-coupon');
 });
